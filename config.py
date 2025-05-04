51f5e8d36155e243d2a01dcd7dfd6cf15acb0707
MuhamadDaffaNashrullah <159070749+MuhamadDaffaNashrullah@users.noreply.github.com>
2025-05-04 17:56:05 +0700

    V1

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 class Config:
     DEBUG = False
     TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
 
 class ProductionConfig(Config):
     pass
